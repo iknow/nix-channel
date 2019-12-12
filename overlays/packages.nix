097bc877c85d834891eb583cf90dff5f7ff08a15
Andrew Childs <andrew.childs@bibo.com.ph>
2019-12-12 16:55:39 +0900

    Remove local definition of lorri
    This is now available in the upstream channels.
    
    The patch was working around an upstream issue [1], which is now
    marked as closed.
    
    [1] https://github.com/target/lorri/issues/98

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -1,17 +1,6 @@
 self: super: {
   branch-server-ssh = super.callPackage ./branch-server-ssh {};
 
-  lorri = let
-    src = builtins.fetchGit {
-      url = "https://github.com/target/lorri";
-      ref = "rolling-release";
-    };
-  in (import src {
-    inherit src;
-  }).overrideAttrs(attrs: {
-    patches = (attrs.patches or []) ++ [./lorri-ignore-ssl.patch];
-  });
-
   phraseapp_updater = super.callPackage ./phraseapp_updater {};
 
   neovim-nightly = super.callPackage ./neovim-nightly.nix {};
