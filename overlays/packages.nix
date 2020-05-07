2bfc7ffb35b7ff7478a3c13ce84378e71fe5b23e
Chris Andreae <chris@bibo.com.ph>
2020-05-07 13:19:51 +0900

    Add branchctl

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -4,4 +4,11 @@ self: super: {
   phraseapp_updater = super.callPackage ./phraseapp_updater {};
 
   neovim-nightly = super.callPackage ./neovim-nightly.nix {};
+
+  branchctl = let
+    src = builtins.fetchGit {
+      url = "git@github.com:iknow/branchctl";
+      ref = "master";
+    };
+  in super.callPackage "${src}/nix/branchctl.nix" {};
 }
