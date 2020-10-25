4af5e73c12c7dd9acfd10fdc47203f52f02448fe
Chris Andreae <chris@bibo.com.ph>
2020-10-25 13:36:14 +0900

    Fetching from branchctl repo needs to be lazy
    so that using the iknow channel overlay doesn't require github keys

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,16 @@
 let
-  overlays = [
-    (let
-      src = builtins.fetchGit {
-        url = "git@github.com:iknow/branchctl";
-        ref = "master";
-      };
-    in import "${src}/nix/overlay.nix")
-    (import ./packages.nix)
-  ];
+  branchCtlRepo = fetchGit { url = "git@github.com:iknow/branchctl"; ref = "master"; };
 in
-self: super:
-with super.lib; (foldl' composeExtensions (self': super': {}) overlays) self super
+self: super: {
+  branch-server-ssh = super.callPackage ./branch-server-ssh {};
+
+  phraseapp_updater = super.callPackage ./phraseapp_updater {};
+
+  neovim-nightly = super.callPackage ./neovim-nightly.nix {};
+
+  branchctl = self.callPackage "${branchCtlRepo}/nix/branchctl.nix" {};
+
+  branchctlPlugins = {
+    branchctl-secret-gpg = self.callPackage "${branchCtlRepo}/plugins/branchctl-secret-gpg" {};
+  };
+}
--- overlays/packages.nix
+++
@@ -1,7 +0,0 @@
-self: super: {
-  branch-server-ssh = super.callPackage ./branch-server-ssh {};
-
-  phraseapp_updater = super.callPackage ./phraseapp_updater {};
-
-  neovim-nightly = super.callPackage ./neovim-nightly.nix {};
-}
