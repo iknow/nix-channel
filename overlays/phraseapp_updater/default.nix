f399fa85e46e71fdab18c4f457920793676fdbdb
Chris Andreae <chris@bibo.com.ph>
2023-02-20 13:54:20 +0900

    remove patched dependency for phraseapp_updater 3.0.0

--- overlays/phraseapp_updater/default.nix
+++ overlays/phraseapp_updater/default.nix
@@ -1,4 +1,4 @@
-{ lib, bundlerApp, ruby, defaultGemConfig, makeWrapper, fetchpatch }:
+{ lib, bundlerApp, ruby, defaultGemConfig, makeWrapper }:
 
 bundlerApp rec {
   pname = "phraseapp_updater";
@@ -13,19 +13,6 @@ bundlerApp rec {
   postBuild = ''
     wrapProgram $out/bin/phraseapp_updater --prefix PATH : ${lib.makeBinPath [ ruby ]}
   '';
-
-  gemConfig = defaultGemConfig // {
-    # HashDiff presently has an intrusive unconditional deprecation message.
-    # We use it correctly, we don't need to see the message.
-    hashdiff = attrs: {
-      patches = [
-        (fetchpatch {
-          url = "https://github.com/liufengyun/hashdiff/commit/2dc6adc71739c4aec23c1a946e25ea36f8c69f58.diff";
-          sha256="1pibvad2fxpg30pjdhk2q8ly4pfn7yiyv331zdvw9viqickx225j"; })
-      ];
-      dontBuild = false;
-    };
-  };
 
   meta = with lib; {
     description = "A tool for merging data on PhraseApp with local changes (usually two git revisions)";
