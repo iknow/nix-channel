4435cbb67f79e4b0ab9bce8e0adab55b6aa96795
Chris Andreae <chris@bibo.com.ph>
2023-02-20 13:45:32 +0900

    Update phraseapp_updater

--- overlays/phraseapp_updater/gemset.nix
+++ overlays/phraseapp_updater/gemset.nix
@@ -1,31 +1,74 @@
 {
   deep_merge = {
+    groups = ["default"];
+    platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1q3picw7zx1xdkybmrnhmk2hycxzaa0jv4gqrby1s90dy5n7fmsb";
+      sha256 = "0fjn4civid68a3zxnbgyjj6krs3l30dy8b4djpg6fpzrsyix7kl3";
       type = "gem";
     };
-    version = "1.2.1";
+    version = "1.2.2";
+  };
+  ethon = {
+    dependencies = ["ffi"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "17ix0mijpsy3y0c6ywrk5ibarmvqzjsirjyprpsy3hwax8fdm85v";
+      type = "gem";
+    };
+    version = "0.16.0";
+  };
+  ffi = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1862ydmclzy1a0cjbvm8dz7847d9rch495ib0zb64y84d3xd4bkg";
+      type = "gem";
+    };
+    version = "1.15.5";
   };
   hashdiff = {
     groups = ["default"];
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1ncwxv7jbm3jj9phv6dd514463bkjwggxk10n2z100wf4cjcicrk";
+      sha256 = "1nynpl0xbj0nphqx1qlmyggq58ms1phf5i03hk64wcc0a17x1m1c";
       type = "gem";
     };
-    version = "0.4.0";
+    version = "1.0.1";
+  };
+  json = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0nalhin1gda4v8ybk6lq8f407cgfrj6qzn234yra4ipkmlbfmal6";
+      type = "gem";
+    };
+    version = "2.6.3";
+  };
+  link-header-parser = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0xq8h3bg3f3jjrk639ngw1zaijcip1yfcvd8q9lss3fd3d89aibg";
+      type = "gem";
+    };
+    version = "4.0.0";
   };
   multi_json = {
     groups = ["default"];
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "0xy54mjf7xg41l8qrg1bqri75agdqmxap9z466fjismc1rn2jwfr";
+      sha256 = "0pb1g1y3dsiahavspyzkdy39j4q377009f6ix0bh1ag4nqw43l0z";
       type = "gem";
     };
-    version = "1.14.1";
+    version = "1.15.0";
   };
   oj = {
     source = {
@@ -40,29 +83,32 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "17b127xxmm2yqdz146qwbs57046kn0js1h8synv01dwqz2z1kp2l";
+      sha256 = "07vnk6bb54k4yc06xnwck7php50l09vvlw1ga8wdz0pia461zpzb";
       type = "gem";
     };
-    version = "1.19.2";
+    version = "1.22.1";
   };
-  phraseapp-ruby = {
-    source = {
-      remotes = ["https://rubygems.org"];
-      sha256 = "14n2hhwjn32xk0qk6rprs3awnrddhnd4zckyd0a4j8lv8k648pnn";
-      type = "gem";
-    };
-    version = "1.6.0";
-  };
-  phraseapp_updater = {
-    dependencies = ["deep_merge" "hashdiff" "multi_json" "oj" "parallel" "phraseapp-ruby" "thor"];
+  phrase = {
+    dependencies = ["json" "link-header-parser" "typhoeus"];
     groups = ["default"];
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1rz5m4jycqkh2s2ins53shc1k06xq8gj5d0vin9s8kdg82kan2d2";
+      sha256 = "1x4q71w4z0s3acg6glvs7bbd9p4nf0r5ida9aa9pc4gkmvwbdxrf";
       type = "gem";
     };
-    version = "2.1.1";
+    version = "2.8.3";
+  };
+  phraseapp_updater = {
+    dependencies = ["deep_merge" "hashdiff" "multi_json" "oj" "parallel" "phrase" "thor"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "04fcsnqgmlxnisvn1jcv9zrc62s02gv5g1fw11iqvv02hlfh2pdr";
+      type = "gem";
+    };
+    version = "3.0.0";
   };
   thor = {
     source = {
@@ -71,5 +117,16 @@
       type = "gem";
     };
     version = "0.20.3";
+  };
+  typhoeus = {
+    dependencies = ["ethon"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1m22yrkmbj81rzhlny81j427qdvz57yk5wbcf3km0nf3bl6qiygz";
+      type = "gem";
+    };
+    version = "1.4.0";
   };
 }
