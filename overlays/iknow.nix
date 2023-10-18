87451ea82c13fb1c15d78ebe59f339186d879914
Thomas Dy <thatsmydoing@gmail.com>
2023-10-18 16:05:17 +0900

    overlays: add dprint

--- overlays/iknow.nix
+++ overlays/iknow.nix
@@ -3,4 +3,6 @@ final: prev: {
   phraseapp_updater = final.callPackage ./phraseapp_updater {};
 
   inherit (final.callPackage ./yarn {}) yarn-iknow berry2nix;
+
+  dprintForConfig = dprintJson: final.callPackage ./dprint.nix { inherit dprintJson; };
 }
